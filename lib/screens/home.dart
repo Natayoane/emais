0f33e50149129dcf4ea85f0b853d41fd03bd7705
Natalia Tayoane <nataliatayoane@gmail.com>
2022-10-03 01:14:35 -0300

    Tela de home finalizada

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 
 class HomeWidget extends StatelessWidget {
@@ -5,32 +7,110 @@ class HomeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // ignore: prefer_const_declarations
-    final urlImage = 'images/fundo.png';
-
     return Scaffold(
       body: Container(
-          // ignore: prefer_const_constructors
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: NetworkImage(urlImage),
-              fit: BoxFit.cover,
-            ),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/fundo.png"),
+            fit: BoxFit.cover,
           ),
-          child:
-              // ignore: prefer_const_literals_to_create_immutables
-              Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Image.asset(
-                'images/logo.png',
-                height: 219,
-                width: 219,
+        ),
+        padding: const EdgeInsets.only(top: 60, left: 40, right: 40),
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              width: 200,
+              height: 200,
+              child: Image.asset("images/logo.png"),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            const Text(
+              "Que bom te ver aqui!",
+              // ignore: unnecessary_const
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 20,
               ),
-              const Text("Que bom te ver aqui!!")
-            ],
-          )),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 150,
+            ),
+            Container(
+              height: 55,
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 91, 205, 250),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(32),
+                ),
+              ),
+              child: SizedBox.expand(
+                child: FlatButton(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: <Widget>[
+                      const Text(
+                        "Cadastrar-se",
+                        // ignore: unnecessary_const
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                  onPressed: () {
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(
+                    //     builder: (context) => HomePage(),
+                    //   ),
+                    // );
+                  },
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Container(
+              height: 55,
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 91, 205, 250),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(32),
+                ),
+              ),
+              child: SizedBox.expand(
+                child: FlatButton(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: <Widget>[
+                      const Text(
+                        "Entrar",
+                        // ignore: unnecessary_const
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
-
-// child: Text('Que bom ver você aqui!'),
